93563226cdafc29f0729561d9c6b636789b23857
maalik420 <49335563+maalik420@users.noreply.github.com>
2025-04-20 17:18:39 +0530

    Create numberguessinggame.py
    added source code

---
+++ numberguessinggame.py
@@ -0,0 +1,31 @@
+import art
+import random
+
+def check_the_guess_number(guess_by_player, guessed_by_host,):
+    if guess_by_player == guessed_by_host:
+        return 'You got it!!'
+    elif guess_by_player < guessed_by_host:
+        return 'No, this number is too low'
+    else:
+        return 'No, this number is too high'
+
+def guessing_game(no_of_guesses,):
+    answer = ''
+    while no_of_guesses > 0 and answer != 'You got it!!':
+        guess_by_player = int(input('Now tell me what number I guessed  between 1 and 100'))
+        answer = check_the_guess_number(guess_by_player, guessed_by_host,)
+        print(answer)
+        no_of_guesses -= 1
+
+print(art.logo)
+print('Welcome to the number guessing game!!')
+guessed_by_host = random.randrange(1, 100)
+print('I have guessed the number between 1 and 100. Let\'s play guessing...')
+choice = input('Choose difficulty level: easy or hard')
+
+if choice.lower() == 'easy':
+    no_of_guesses = 10
+    guessing_game(no_of_guesses)
+else:
+    no_of_guesses = 5
+    guessing_game(no_of_guesses)
